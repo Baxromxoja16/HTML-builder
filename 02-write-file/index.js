3d8be9b59eb11cf8006b0852f25d8dd087ebc652
baxromxoja16 <aglamof@mail.ru>
2022-11-04 17:06:31 +0500

    feat: 03-files-in-folder completed
    and fix write file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,5 +6,16 @@ var fileName = __dirname + "\\text.txt";
 
 const writeStream = fs.createWriteStream(fileName, "utf-8");
 
-// Input text and watch 
+// Input text and watch
 writeStream.write("Create text");
+
+// Read text
+const readStream = fs.createReadStream(fileName, "utf-8");
+
+readStream.on("readable", () => {
+  var data = readStream.read();
+
+  if (data != null) {
+    console.log(`Created text in ${"text.txt"} file, "${data}" `);
+  }
+});
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,18 @@
 // Importing moduls
 const fs = require("fs");
+const path = require("path");
 
-var file = ["csv", "js", "css", "txt"];
-
-const dirName = __dirname + "\\secret-folder\\"
+const dirName = __dirname + "\\secret-folder\\";
 
 fs.readdir(dirName, { withFileTypes: true }, (err, files) => {
   files.forEach((file) => {
-    console.log(file.name);
+    fs.stat(dirName+file.name, (err, stats) => {
+      var name = file.name.split(".")[0] // file name
+      var ext = file.name.split(".")[1] // file extension 
+
+      if(stats.size !== 0){
+        console.log(`${name} - ${ext} -  ${(stats.size / 1024).toFixed(3)}kb`);
+      }
+    });
   });
 });
