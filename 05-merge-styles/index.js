94e34e12acb0f714401f81f6d4d08af6b88ea4dd
baxromxoja16 <aglamof@mail.ru>
2022-11-05 12:59:12 +0500

    feat: merge styles completed

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const stylePath = path.join(__dirname, "styles");
+const bandleStyle = path.join(__dirname, "project-dist", "bundle.css");
+
+fs.readdir(stylePath, (err, files) => {
+  // watch styles folder
+  files.forEach((val) => {
+    if (path.extname(val) == ".css") {
+      var createRead = new fs.ReadStream(path.join(stylePath, val), "utf-8");
+
+      // read style files
+      createRead.on("readable", () => {
+        var data = createRead.read();
+
+        if (data != null) {
+          var createWrite = fs.createWriteStream(bandleStyle);
+          createWrite.write(data);
+        }
+      });
+    }
+  });
+});
