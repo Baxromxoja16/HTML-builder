07c82d6f0544d876cae231da067757eb5d17ff31
baxromxoja16 <aglamof@mail.ru>
2022-11-05 11:52:04 +0500

    feat: copy derectory compeled

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
 
+const folderNameCopy = __dirname + "\\file-copy";
+const folderName = __dirname + "\\files";
+
+//create folder "file-copy"
+fs.mkdir(folderNameCopy, (err) => {
+  if (err) return err
+  console.log("Directory created successfully!");
+});
+
+// View files 
+fs.readdir(folderName, (err, files) => {
+    
+    files.forEach((file) => {
+    // Copy files
+    fs.copyFile((folderName+`\\${file}`),(folderNameCopy+`\\${file}`),(err)=>{
+        if(err) return err
+
+        console.log("Copied successfully");
+    })
+  });
+});
