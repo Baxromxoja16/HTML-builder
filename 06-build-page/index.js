73475d1cf32fac39caadbbcbaa88efeb02ed094c
baxromxoja16 <aglamof@mail.ru>
2022-11-07 15:59:34 +0500

    feat: finally build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,15 +1,22 @@
 const fs = require("fs");
+const fsPromises = require("fs/promises");
 const path = require("path");
 
 const distFolder = path.join(__dirname, "project-dist");
+const distHtmlFile = path.join(__dirname, "project-dist", "index.html");
+var stylesFolder = path.join(__dirname, "styles");
 const assetsFolder = path.join(__dirname, "assets");
+const distAssetsFolder = path.join(distFolder, "assets");
+const componentsFolder = path.join(__dirname, "components");
+const templateFile = path.join(__dirname, "template.html");
 
 fs.mkdir(distFolder,()=>{
     console.log("folder created");
 });
 
-// fs.readdir(assetsFolder,(err,files)=>{
-//     files.forEach(element => {
-//         console.log(element);
-//     });
-// })
+// Create dir in dist assets
+function mkdirFunc(param) {
+  fs.mkdir(path.join(distAssetsFolder, param), { recursive: true }, (err) =>
+    console.log("Directory created successfully in dist assets!")
+  );
+}
